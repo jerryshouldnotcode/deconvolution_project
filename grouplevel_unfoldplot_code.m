d8bea20bf771b95ef863d8a5b575a8c81e66a1b6
moldyv0rt <115601267+jerryshouldnotcode@users.noreply.github.com>
2025-01-30 19:57:22 -0500

    Add files via upload

---
+++ grouplevel_unfoldplot_code.m
@@ -0,0 +1,55 @@
+init_unfold
+
+projectFolder = 'C:\Users\HP\Documents\MATLAB\projects\fixation-label for deconvolution\output_deconvolution';
+
+% Load the data
+filename = 'all_betas.mat';  % Replace with your actual .mat filename
+tmp = load(fullfile(projectFolder, filename));
+ufresult = tmp.ufresult;
+
+%% plot deconv vs nodeconv
+% This does not work with splines 
+cfg = [];
+cfg.channel = 16; % channel location
+cfg.add_intercept = 0;
+cfg.baseline = [min(ufresult.times) 0];
+
+%% figure plotting
+% try
+%     % First attempt with standard plotting
+%     uf_plot2nd(ufresult,cfg);
+% catch
+%     % Alternative plotting approach
+%     g = uf_plotParam(ufresult, 'channel', cfg.channel, ...
+%         'deconv', 1, ...
+%         'baseline', cfg.baseline, ...
+%         'plotSeparate', 'event', ...
+%         'plotCI', 'sem');  % Add confidence intervals using standard error of the mean
+%     g.set_color_options('map', [0 0 1]); % RGB for blue
+%     g.draw();
+% end
+
+% This function calculates a channel matrix that can be used for default cluster-permutation tests
+% using the ept-TFCE toolbox
+ept_tfce_nb = ept_ChN2(ufresult.chanlocs,1); % the 1 to plot
+disp(ept_tfce_nb)
+
+% Average amount of neighbours:
+mean(sum(ept_tfce_nb'>0))
+
+cfg = [];
+
+% excluding eye-movement channels from the Eye-EEG toolbox
+cfg.chan = 1:30; 
+
+cfg.pred = 4; % predictability at a target-word
+
+% subselect channels & predictor  
+data = squeeze(ufresult.beta(cfg.chan,:,cfg.pred,:));
+
+% baseline correction
+data = bsxfun(@minus,data,mean(data(:,ufresult.times<0,:),2));
+
+% do the statistics
+% this is a customized version of the original ept_tfce_diff function
+res = ept_lme_TFCE('data',permute(data,[3 1 2]),'perm_data',500,'channel_neighbours',ept_tfce_nb(cfg.chan,:));
--- intro_unfoldplot_code.m
+++ intro_unfoldplot_code.m
@@ -1,69 +1,75 @@
 init_unfold
 
-% change this later for group data
-EEG = pop_loadset('C:\Users\HP\Documents\MATLAB\projects\fixation-label for deconvolution\output\modified_RLGL011_ICA.set');
-EEG = eeg_checkset(EEG);
+% receiving files from output of sequence_labeling code
+datasetFolder = 'C:\Users\HP\Documents\MATLAB\projects\fixation-label for deconvolution\output_sequence_labeling';
+datasetFiles = dir(fullfile(datasetFolder, '*.set')); % Get all .set files
+numDatasets = length(datasetFiles);
 
-% initializing array to subset EEG data we are observing 
-cfgDesign = [];
+% Preallocate storage for uf_results parameters
+allResults = struct();
+allResults.datasetNames = strings(1, numDatasets);
 
-% defining event types for low and high constraint 
-cfgDesign.eventtypes = {'1312','1311'};
+for i = 1: numDatasets
+    EEG = pop_loadset('filename', datasetFiles(i).name, 'filepath', datasetFolder);
+    EEG = eeg_checkset(EEG);
+    
+    % initializing array to subset EEG data we are observing 
+    cfgDesign = [];
+    
+    % defining event types for low and high constraint 
+    cfgDesign.eventtypes = {'1312','1311'};
+    
+    % explicit formula: y = 1 + type + fix_type + type:fix_type {interaction effect}
+    cfgDesign.formula = {'y ~ 1 + cat(type)*cat(fix_type)'}; 
+    
+    % Make sure both types are explicitly included
+    cfgDesign.categorical = {'type', {'1311', '1312'},...
+                            'fix_type', {'single', 'first_refix', 'second_refix'}}; 
+    
+    % initializes sparse matrix with the events and variables stated
+    EEG = uf_designmat(EEG,cfgDesign);
+    
+    % defines the timeperiod we are observing over (-300ms to 800ms)
+    cfgTimeexpand = [];
+    cfgTimeexpand.timelimits = [-.3,0.8];
+    EEG = uf_timeexpandDesignmat(EEG,cfgTimeexpand);
+    
+    % identify "bad" intervals artifacts in the continuous EEG...
+    winrej = uf_continuousArtifactDetect(EEG,'amplitudeThreshold',100,'channels',16); % important to exclude synchronized eye-tracker channels for this step (if they exist)
+    
+    % ...and remove them from time-expanded design matrix
+    EEG = uf_continuousArtifactExclude(EEG,struct('winrej',winrej));
+    
+    % replacing nans in the design matrix with zeroes
+    % EEG.unfold.Xdc(isnan(EEG.unfold.Xdc)) = 0;
+    
+    % Fitting deconvolution model
+    EEG = uf_glmfit(EEG,'channel',16); 
+    
+    % new epoched dataset, fitting non-deconvolved model
+    EEG_epoch = uf_epoch(EEG,struct('winrej',winrej,'timelimits',cfgTimeexpand.timelimits));
+    
+    % condensing dataset
+    ufresult= uf_condense(EEG_epoch);
 
-% explicit formula: y = 1 + type + fix_type + type:fix_type {interaction effect}
+    % Store relevant fields from ufresult
+    allResults.datasetNames(i) = datasetFiles(i).name;
+    allResults.beta{i} = ufresult.beta;       % Model coefficients
+    allResults.times{i} = ufresult.times;     % Timepoints
+    allResults.chanlocs{i} = ufresult.chanlocs; % Channel locations
+    allResults.param{i} = ufresult.param;     % Model parameters
+    allResults.unfold{i} = ufresult.unfold;   % Extra Unfold data (if needed)
 
-cfgDesign.formula = {'y ~ 1 + cat(type)*cat(fix_type)'}; 
+    % debugging
+    % display(ufresult)
+    
+    % Debug: Print the structure of ufresult to see what's available
+    disp('Structure of ufresult:')
+    disp(ufresult)
+end
 
-% Make sure both types are explicitly included
-cfgDesign.categorical = {'type', {'1311', '1312'},...
-                        'fix_type', {'single', 'first_refix', 'second_refix'}}; 
+outputFolder = 'C:\Users\HP\Documents\MATLAB\projects\fixation-label for deconvolution\output_deconvolution';
 
-% initializes sparse matrix with the events and variables stated
-EEG = uf_designmat(EEG,cfgDesign);
-
-% defines the timeperiod we are observing over (-300ms to 800ms)
-cfgTimeexpand = [];
-cfgTimeexpand.timelimits = [-.3,0.8];
-EEG = uf_timeexpandDesignmat(EEG,cfgTimeexpand);
-
-% identify "bad" intervals artifacts in the continuous EEG...
-winrej = uf_continuousArtifactDetect(EEG,'amplitudeThreshold',100,'channels',16); % important to exclude synchronized eye-tracker channels for this step (if they exist)
-
-% ...and remove them from time-expanded design matrix
-EEG = uf_continuousArtifactExclude(EEG,struct('winrej',winrej));
-
-% replacing nans in the design matrix with zeroes
-% EEG.unfold.Xdc(isnan(EEG.unfold.Xdc)) = 0;
-
-% Fitting deconvolution model
-EEG = uf_glmfit(EEG,'channel',16); 
-
-% new epoched dataset, fitting non-deconvolved model
-EEG_epoch = uf_epoch(EEG,struct('winrej',winrej,'timelimits',cfgTimeexpand.timelimits));
-
-% condensing dataset
-ufresult= uf_condense(EEG_epoch);
-
-% debugging
-display(ufresult)
-
-% Debug: Print the structure of ufresult to see what's available
-disp('Structure of ufresult:')
-disp(ufresult)
-
-% Plot with more explicit parameters
-g1 = uf_plotParam(ufresult, 'channel', 16, ...
-    'deconv', 1, ...
-    'baseline', [ufresult.times(1) 0], ...
-    'plotSeparate', 'event');
-g1.set_color_options('map', [0 0 1]); % RGB for blue
-g1.update();
-
-% Optional: Add second plot for non-deconvoluted betas
-% g2 = uf_plotParam(ufresult, 'channel', 16, ...
-%     'deconv', 0, ...
-%     'baseline', [ufresult.times(1) 0], ...
-%     'plotParam', {'type_1311', 'type_1312'}, ... % Explicitly specify both types
-%     'gramm', g1);
-% g2.set_color_options('map', [1 0 0]); % RGB for red
-% g2.update();
+% Save all betas in one .mat file
+save(fullfile(outputFolder, 'all_betas.mat'), 'ufresult');
+disp(['All dataset parameters in ', datasetFolder, ' saved!']);
